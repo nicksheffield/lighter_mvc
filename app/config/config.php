8904a2894a78ef754140112fdb5fce6acc43b0f1
Nick Sheffield <numbereft@gmail.com>
2014-06-16 14:17:34 +1200

    Improved spacing and removed db reference

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
-$config['default_controller'] = 'home';
-$config['base_url']           = '/github/mvc/public/';
+$config['default_controller']  = 'home';
+$config['base_url']            = '/github/mvc/public/';
 
-$config['autoload']['libs']   = array('database');
-$config['autoload']['models'] = array();
+$config['autoload']['libs']    = array('database', 'form');
+$config['autoload']['models']  = array();
 
-$config['db']['hostname'] = 'localhost';
-$config['db']['username'] = 'root';
-$config['db']['password'] = '';
-$config['db']['database'] = 'cms_b';
+$config['db']['hostname']      = 'localhost';
+$config['db']['username']      = 'root';
+$config['db']['password']      = '';
+$config['db']['database']      = '';

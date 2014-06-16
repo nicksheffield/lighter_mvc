452111b8713ef8ce63cc94e3883a2ce12add8abf
Nick Sheffield <numbereft@gmail.com>
2014-06-16 16:45:58 +1200

    Add print_r of URL::parts() to homepage

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -2,4 +2,5 @@
 	<h1>Welcome to my first MVC framework</h1>
 	<p><i>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i></p>
 	<p>Harum rerum eum facilis hic quos dignissimos asperiores suscipit consequatur voluptates alias facere quo animi ad itaque nesciunt at ipsa distinctio magni aspernatur sint. Expedita perspiciatis iste fugit at laudantium.</p>
+	<pre>URL::parts() => <?php print_r(URL::parts()) ?></pre>
 </div>

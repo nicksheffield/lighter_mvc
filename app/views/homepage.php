5f0c7398226955cfb130943028cf332f417177b4
Nick Sheffield <numbereft@gmail.com>
2014-06-16 17:33:16 +1200

    Improved example on home page

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -2,5 +2,8 @@
 	<h1>Welcome to my first MVC framework</h1>
 	<p><i>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</i></p>
 	<p>Harum rerum eum facilis hic quos dignissimos asperiores suscipit consequatur voluptates alias facere quo animi ad itaque nesciunt at ipsa distinctio magni aspernatur sint. Expedita perspiciatis iste fugit at laudantium.</p>
+	
+
+	<p>For a good example of the URL::parts() function, try going to this url: <a href="home/index/yo/waddup/dude">/home/index/yo/waddup/dude</a></p>
 	<pre>URL::parts() => <?php print_r(URL::parts()) ?></pre>
 </div>

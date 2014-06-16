a78e0232e9d91a7739c61f040e84c48a9f35dbe1
Nick Sheffield <numbereft@gmail.com>
2014-06-16 17:45:23 +1200

    Refactored routing

--- sys/core/routing.php
+++ sys/core/routing.php
@@ -1,59 +1,45 @@
 <?php
 
-# If no specific page was requested...
-if(!isset($_GET['page'])){
-	# Then we use the default one
-	$page = $config['default_controller'];
-}else{
-	# If a page was requested, then we have it.
-	$page = $_GET['page'];
+$controller = '';
+$method     = '';
+$params     = [];
+
+$parts = URL::parts();
+
+switch(count($parts)){
+	case 0:
+		$controller = $config['default_controller'];
+		$method     = 'index';
+		break;
+	case 1:
+		$controller = $parts[0];
+		$method     = 'index';
+		break;
+	case 2:
+		$controller = $parts[0];
+		$method     = $parts[1];
+		break;
+	default:
+		$controller = $parts[0];
+		$method     = $parts[1];
+		$params     = array_slice($parts, 2);
+		break;
 }
 
-# if any routes are set for this page, use that instead.
-if(isset($routes[$page])){
-	$page = $routes[$page];
-}
+if(file_exists(APP_URL.'/controllers/'.$controller.'.php')){
+	require_once APP_URL.'/controllers/'.$controller.'.php';
 
-# Figure out what is controller, what is method, and what is param from the url
-$segments = explode('/', $page);
+	$c = new $controller($registry);
 
-# the first segment is always the controller
-$controller = $segments[0];
-
-# If a second segment exists, it's going to be the method
-if(count($segments)>1){
-	$method = $segments[1];
-}else{
-	$method = 'index';
-}
-
-# any further segments are params
-$params = array();
-
-if(count($segments)>2){
-	# start the for loop at 2. clever.
-	for($i=2; $i<count($segments); $i++){
-		$params[] = $segments[$i];
-	}
-}
-
-
-# Check if the page exists and that it isn't a private method (starts with an underscore)
-if(file_exists(APP_URL.'/controllers/'.$controller.'.php') && strpos($method, '_') !== 0){
-	require_once(APP_URL.'/controllers/'.$controller.'.php');
-
-	$loaded_controller = new $controller($registry);
-
-	if(method_exists($loaded_controller, $method)){
-		call_user_func_array(array($loaded_controller, $method), $params);
+	if($c){
+		if(method_exists($c, $method)){
+			call_user_func_array(array($c, $method), $params);
+		}else{
+			include(APP_URL.'/errors/404.php');
+		}
 	}else{
 		include(APP_URL.'/errors/404.php');
 	}
-	
 }else{
 	include(APP_URL.'/errors/404.php');
 }
-
-// echo 'controller: '.$controller.'<br>';
-// echo 'method: '.$method.'<br>';
-// echo 'params: '; print_r($params);

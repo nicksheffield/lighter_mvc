fe1e620c7b84b39859d70187e1daf6de113b90e1
Nick Sheffield <numbereft@gmail.com>
2014-07-01 18:08:38 +1200

    added % to allowed url chars

--- sys/core/routing.php
+++ sys/core/routing.php
@@ -7,11 +7,12 @@ $params     = array();
 # intercept the route process by checking if we should be using a custom route instead
 
 foreach(Registry::$routes as $route => $newpath){
-	$t_route = '/^'.str_replace(array('/',':num',':any'), array('\/','\d{0,}','[A-z0-9\-]{0,}'), $route).'$/';
+	$t_route = '/^'.str_replace(array('/',':num',':any'), array('\/','\d{0,}','[A-z0-9\-\%]{0,}'), $route).'$/';
 
 	if(URL::string() == $route){
 
 		$_GET['page'] = $newpath;
+		break;
 
 	}else if(preg_match($t_route, URL::string())){
 

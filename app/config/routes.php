3165478f19a198d0f84094e98ab23718aee047fb
Nick Sheffield <numbereft@gmail.com>
2014-07-01 18:08:45 +1200

    Added wildcard docs

--- app/config/routes.php
+++ app/config/routes.php
@@ -11,12 +11,24 @@
 *	This means if a user tries to access yourwebsite.com/login 
 *	it will load the home controller and then run the login method
 *
-*	@todo wildcards
+*	Wildcards are also possible.
+*
+*	For example, if you want to edit a user, you might have a controller already called "admin",
+*	with a method inside that called "edit_user", but want to url of that page to look like
+*	http://website.com/edit/user/9
+*
+*	Even if you don't have an "edit" controller, with a "user" function, you can still simulate that
+*	by creating a route like:
+*
+*	$routes['edit/user/:num'] = 'admin/edit_user/:num';
+*
+*	:num can be replaced by any digit
+*	:any can be replaced by any alphanumeric character (a-z, A-Z, 0-9), or hyphen (-) and percent (%)
 *
 */
 
-$routes['login']    = 'home/login';
-$routes['wtf']      = 'blah';
+$routes['login']     = 'home/login';
+$routes['wtf']       = 'blah';
 $routes['page/:num'] = 'page/view/:num';
 $routes['page/:any'] = 'page/view_by_name/:any';
 $routes['user/:any'] = 'user/:any';

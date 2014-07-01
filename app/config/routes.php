d776ccaf198655eda18ab508034655a6a0bb51d3
Nick Sheffield <numbereft@gmail.com>
2014-07-01 17:51:23 +1200

    Added another test route

--- app/config/routes.php
+++ app/config/routes.php
@@ -18,4 +18,5 @@
 $routes['login']    = 'home/login';
 $routes['wtf']      = 'blah';
 $routes['page/:num'] = 'page/view/:num';
+$routes['page/:any'] = 'page/view_by_name/:any';
 $routes['user/:any'] = 'user/:any';
--- app/controllers/page.php
+++ app/controllers/page.php
@@ -18,4 +18,21 @@ class Page extends Controller{
 		Load::view('footer');
 	}
 
+	function view_by_name($name){
+
+		Load::model('page_model');
+
+		$page = new Page_model();
+
+		$page->load_by_name($name);
+
+		$data['name']    = $page->name;
+		$data['content'] = $page->content;
+
+		Load::view('header');
+		Load::view('page', $data);
+		Load::view('footer');
+
+	}
+
 }

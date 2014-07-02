ca191482d558c92e4c046ae9980bc9efd88e48ee
Nick Sheffield <numbereft@gmail.com>
2014-07-02 14:34:58 +1200

    Added Load::library()

--- sys/libraries/load.php
+++ sys/libraries/load.php
@@ -8,6 +8,12 @@ class Load{
 		}
 	}
 
+	public static function library($name){
+		if(file_exists(APP_URL.'/libraries/'.$name.'.php')){
+			require_once(APP_URL.'/libraries/'.$name.'.php');
+		}
+	}
+
 	public static function view($page_name_xyz, $data = array()){
 		if(file_exists(APP_URL.'/views/'.$page_name_xyz.'.php')){
 			if(count($data)){

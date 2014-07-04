adb5947f38022bd8d912dcd845d42b16edf026dd
Nick Sheffield <numbereft@gmail.com>
2014-07-04 12:13:35 +1200

    Stopped sidebar moving

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -2,7 +2,7 @@ var sidebar = document.querySelector('.sidebar');
 
 window.onscroll = function(e){
 	if(window.scrollY > 45){
-		sidebar.style.position = 'fixed';
+		// sidebar.style.position = 'fixed';
 	}else{
 		sidebar.style.position = 'absolute';
 	}
